671fde152576a1c95b9f158939d317ad914db54a
prageeth <prageethravindra1432@gmail.com>
2024-11-29 10:20:27 +0530

    Setup  Configurations

---
+++ Bookify/Data/LibraryContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=library.db");
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
