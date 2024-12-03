c499be1b45fc2611a9e2bb109946daf92ebed348
prageeth <prageethravindra1432@gmail.com>
2024-12-03 13:20:40 +0530

    confi changes

--- Bookify/obj/Debug/net8.0/Bookify.AssemblyInfo.cs
+++ Bookify/obj/Debug/net8.0/Bookify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394e520a8eaa99a3705f066749036b32d9cc47f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6128d1c89310a670366ca11175c73333101d57b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
